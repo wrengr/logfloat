151b09bc07ab55b90de68921be28ca8911b77ab1
wren gayle romano <wren@community.haskell.org>
2008-08-17 00:00:51 -0400

    Data.Number.Transfinite: documentation tweaking
    darcs-hash:370bbdbebca622113538484c662b0cff02acb98a

--- Data/Number/Transfinite.hs
+++ Data/Number/Transfinite.hs
@@ -15,15 +15,20 @@
 -- representations for transfinite values. The idea originated from
 -- the IEEE-754 floating-point special values, used by
 -- "Data.Number.LogFloat". However not all 'Fractional' types
--- necessarily support transfinite values.
+-- necessarily support transfinite values. In particular, @Ratio@
+-- types including 'Rational' do not have portable representations.
 -- 
--- In particular, @Ratio@ types including 'Rational' do not have
--- portable representations. For the Glasgow compiler (GHC 6.8.2),
--- "GHC.Real" defines @1%0@ and @0%0@ as representations for
--- 'infinity' and 'notANumber', but most operations on them will
--- yield exceptions. Hugs (September 2006) stays closer to the
--- haskell98 spec and offers no way of constructing those values,
--- yielding exceptions if attempted.
+-- For the Glasgow compiler (GHC 6.8.2), "GHC.Real" defines @1%0@
+-- and @0%0@ as representations for 'infinity' and 'notANumber',
+-- but most operations on them will raise exceptions. If 'toRational'
+-- is used on an infinite floating value, the result is a rational
+-- with a numerator sufficiently large that it will overflow when
+-- converted back to a @Double@. If used on NaN, the result would
+-- convert back as 'negativeInfinity'.
+-- 
+-- Hugs (September 2006) stays closer to the haskell98 spec and
+-- offers no way of constructing those values, raising arithmetic
+-- overflow errors if attempted.
 ----------------------------------------------------------------
 module Data.Number.Transfinite (Transfinite(..)) where
 
@@ -36,8 +41,8 @@ import qualified Prelude (isInfinite, isNaN)
 -- able to represent transfinite values such as infinity itself.
 -- This class is for types which are able to represent such values.
 -- Notably, this class does not require the type to be 'Fractional'
--- since integral types could also have representations for transfinite
--- values.
+-- nor 'Floating' since integral types could also have representations
+-- for transfinite values.
 --
 -- In particular, this class extends the 'Ord' projection to have
 -- a maximum value 'infinity' and a minimum value 'negativeInfinity',
@@ -45,29 +50,41 @@ import qualified Prelude (isInfinite, isNaN)
 -- laws regarding @infinity@ and @negativeInfinity@ should pertain
 -- (e.g. @negate infinity = negativeInfinity@, @infinity + x =
 -- infinity@ when @x@ is finite, etc.). Additionally, @infinity -
--- infinity@ (@0\/0@, and @infinity\/infinity@ if @Fractional@)
--- should return @notANumber@. Any operations on @notANumber@ should
--- also return @notANumber@, and any ordering comparison on
--- @notANumber@ should return @False@.
+-- infinity@ should return @notANumber@ (as should @0\/0@ and
+-- @infinity\/infinity@ if the type is @Fractional@). Any operations
+-- on @notANumber@ should also return @notANumber@, and any equality
+-- or ordering comparison on @notANumber@ must return @False@.
 --
 -- Minimum complete definition is @infinity@, @isInfinite@, and
 -- @isNaN@.
 
 class (Num a, Ord a) => Transfinite a where
     
+    -- | A transfinite value which is greater than all finite values.
+    -- Adding or subtracting any finite value is a no-op. As is
+    -- multiplying by any non-zero positive value (including
+    -- @infinity@), and dividing by any non-zero positive finite
+    -- value.
     infinity         :: a
     
+    -- | A transfinite value which is less than all finite values.
+    -- Obeys all the same laws as @infinity@ with the appropriate
+    -- changes for the sign difference.
     negativeInfinity :: a
     negativeInfinity  = negate infinity
     
+    -- | An exceptional transfinite value for dealing with undefined
+    -- results when manipulating infinite values. Since NaN shall
+    -- return false for all ordering and equality operations, there
+    -- may be more than one machine representation of this `value'.
     notANumber       :: a
     notANumber        = infinity - infinity
     
-    -- | This should return true for both @infinity@ and
-    -- @negativeInfinity@
+    -- | Return true for both @infinity@ and @negativeInfinity@,
+    -- false for all other values.
     isInfinite :: a -> Bool
     
-    -- | This should return true only for @notANumber@
+    -- | Return true only for @notANumber@.
     isNaN      :: a -> Bool
 
 
